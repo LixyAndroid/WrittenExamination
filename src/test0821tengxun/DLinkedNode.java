416a4794217a4d2c7291adc3bac2b340eedead21
LixyAndroid <lixy7811@gmail.com>
2020-08-24 12:09:14 +0800

    LRU

---
+++ src/test0821tengxun/DLinkedNode.java
@@ -0,0 +1,13 @@
+package test0821tengxun;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/8/24 12:06
+ */
+class DLinkedNode {
+    int key;
+    int value;
+    DLinkedNode pre;
+    DLinkedNode post;
+}
+
---
+++ src/test0821tengxun/LRUCache.java
@@ -0,0 +1,108 @@
+package test0821tengxun;
+
+import java.util.Hashtable;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/8/24 12:05
+ */
+public class LRUCache {
+
+    private Hashtable<Integer, DLinkedNode>
+            cache = new Hashtable<Integer, DLinkedNode>();
+    private int count;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.count = 0;
+        this.capacity = capacity;
+
+        head = new DLinkedNode();
+        head.pre = null;
+
+        tail = new DLinkedNode();
+        tail.post = null;
+
+        head.post = tail;
+        tail.pre = head;
+    }
+
+    public int get(String key) {
+
+        DLinkedNode node = cache.get(key);
+        if(node == null){
+            return -1; // should raise exception here.
+        }
+
+        // move the accessed node to the head;
+        this.moveToHead(node);
+
+        return node.value;
+    }
+
+
+    public void set(int key, int value) {
+        DLinkedNode node = cache.get(key);
+
+        if(node == null){
+
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+
+            this.cache.put(key, newNode);
+            this.addNode(newNode);
+
+            ++count;
+
+            if(count > capacity){
+                // pop the tail
+                DLinkedNode tail = this.popTail();
+                this.cache.remove(tail.key);
+                --count;
+            }
+        }else{
+            // update the value.
+            node.value = value;
+            this.moveToHead(node);
+        }
+    }
+    /**
+     * Always add the new node right after head;
+     */
+    private void addNode(DLinkedNode node){
+        node.pre = head;
+        node.post = head.post;
+
+        head.post.pre = node;
+        head.post = node;
+    }
+
+    /**
+     * Remove an existing node from the linked list.
+     */
+    private void removeNode(DLinkedNode node){
+        DLinkedNode pre = node.pre;
+        DLinkedNode post = node.post;
+
+        pre.post = post;
+        post.pre = pre;
+    }
+
+    /**
+     * Move certain node in between to the head.
+     */
+    private void moveToHead(DLinkedNode node){
+        this.removeNode(node);
+        this.addNode(node);
+    }
+
+    // pop the current tail.
+    private DLinkedNode popTail(){
+        DLinkedNode res = tail.pre;
+        this.removeNode(res);
+        return res;
+    }
+}
+
