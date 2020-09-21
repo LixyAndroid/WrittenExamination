598f3d5f07bda54f6f00fbe2cf9ea8f642ba2eda
LixyAndroid <lixy7811@gmail.com>
2020-09-21 10:23:25 +0800

    小米笔试

---
+++ src/test0908/Main.java
@@ -0,0 +1,32 @@
+package test0908;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+    }
+
+}
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+    //递归反转链表
+    public static ListNode resverseListNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode res = resverseListNode(head.next);
+        head.next.next = head;
+        head.next = null;
+        return res;
+
+    }
+}
---
+++ src/test0908/Test2.java
@@ -0,0 +1,35 @@
+package test0908;
+
+import java.util.Scanner;
+
+/**
+ * @author Li Xuyang
+ * @date 2020/9/8 18:23
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        s = s.trim();
+        int j = s.length() - 1;
+        int i = j;
+        StringBuilder res = new StringBuilder();
+        while (i >= 0) {
+            while (i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+            res.append(s.substring(i + 1, j + 1) + " ");
+
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+            j = i;
+        }
+
+        System.out.println(res.toString().trim());
+
+    }
+
+
+}
